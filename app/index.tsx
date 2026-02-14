2c07e2fb3a2542dc4e7589fa14fc5ebf0d1a189c
goulartoliveiracarloseduardo <goulartoliveiracarloseduardo@gmail.com>
2026-02-14 18:13:11 +0000

    feat: configurar nativewind e tailwind para estilização

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Main from '../components/Main'
+
+export default function Index() {
+  return (
+    <View>
+      <Main />
+    </View>
+  );
+}
---
+++ components/Main.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Main() {
+  return (
+    <View>
+      <Text className="text-2xl font-bold">Acompanhe as</Text>
+      <Text className="text-2xl font-bold">expressões do mundo.</Text>
+    </View>
+  );
+}
