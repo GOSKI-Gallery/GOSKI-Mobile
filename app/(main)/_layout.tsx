99616593f25cc1e5af4c925ea761c769c0e0bf09
goulartoliveiracarloseduardo <goulartoliveiracarloseduardo@gmail.com>
2026-04-09 18:53:49 +0000

    setting files up to implement profile page

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -4,6 +4,7 @@ export default function MainLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" /> 
+      <Stack.Screen name="profile" />
     </Stack>
   );
 }
---
+++ components/profile/UserPosts.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function UserPosts() {
+  return (
+    <View>
+      <Text>user-posts</Text>
+    </View>
+  )
+}
