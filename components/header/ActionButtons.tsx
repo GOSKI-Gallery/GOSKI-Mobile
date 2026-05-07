dbdff977250e612953752c593b160343560dffcd
goulartoliveiracarloseduardo <goulartoliveiracarloseduardo@gmail.com>
2026-05-07 00:42:04 +0000

    fix: notification modal open

--- components/header/ActionButtons.tsx
+++ components/header/ActionButtons.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { Image, TouchableOpacity, View } from "react-native";
 import { useModalStore } from "../../states/useModalStore";
+import NotificationModal from "../notification/NotificationModal";
 
 export default function ActionButtons() {
   const { openCreatePostModal, setNotificationModalVisible } = useModalStore();
 
   return (
-    <View className="flex flex-row justify-between items-center gap-2">
-      <TouchableOpacity testID="add-post-button" onPress={openCreatePostModal}>
-        <Image
-          testID="add-post-icon"
-          source={require("../../assets/icons/add.png")}
-          className="w-8 h-8"
-        />
-      </TouchableOpacity>
+    <>
+      <View className="flex flex-row justify-between items-center gap-2">
+        <TouchableOpacity testID="add-post-button" onPress={openCreatePostModal}>
+          <Image
+            testID="add-post-icon"
+            source={require("../../assets/icons/add.png")}
+            className="w-8 h-8"
+          />
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        testID="notification-bell-button"
-        onPress={() => setNotificationModalVisible(true)}
-      >
-        <Image
-          testID="notification-bell-icon"
-          source={require("../../assets/icons/bell.png")}
-          className="w-8 h-8"
-        />
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          testID="notification-bell-button"
+          onPress={() => setNotificationModalVisible(true)}
+        >
+          <Image
+            testID="notification-bell-icon"
+            source={require("../../assets/icons/bell.png")}
+            className="w-8 h-8"
+          />
+        </TouchableOpacity>
+      </View>
+      <NotificationModal />
+    </>
   );
 }
