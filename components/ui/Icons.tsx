386124cca8db57fcedc183291440bcfb9964e822
Carlos Eduardo Goulart <142110393+CarlosEGoulart@users.noreply.github.com>
2026-07-13 21:02:51 +0000

    style(icons): update CommentIcon to more rounded bubble shape

--- components/ui/Icons.tsx
+++ components/ui/Icons.tsx
@@ -100,7 +100,7 @@ export function CommentIcon({ color = "#18181b", size = 24 }: IconProps) {
   return (
     <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
       <Path
-        d="M21 2H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7l3 3 3-3h5c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H3V4h18v12z"
+        d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.17L4 17.17V4h16v12z"
         fill={color}
       />
     </Svg>
